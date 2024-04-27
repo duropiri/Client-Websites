b624474c0fbf6ba64569b28a0395c605707c0396
Duro <duro.dp02@gmail.com>
2024-04-27 03:28:25 -0600

    ~mobile menu

--- Client Websites/Dentist Template/dentist-headless/src/components/header.tsx
+++ Client Websites/Dentist Template/dentist-headless/src/components/header.tsx
@@ -307,12 +307,12 @@ export default function Header() {
       {/* Mobile Menu */}
       <Dialog
         as="div"
-        className="lg:hidden"
+        className="lg:hidden z-50"
         open={mobileMenuOpen}
         onClose={setMobileMenuOpen}
       >
         <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+        <Dialog.Panel className="fixed inset-y-0 right-0 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 z-[9999]">
           {/* Company Logo */}
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
